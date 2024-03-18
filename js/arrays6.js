2786a316071224427dc9fad75024b5801db29eb2
Cr1stal52 <156699952+Cr1stal52@users.noreply.github.com>
2024-03-18 21:25:36 +0300

    task6

---
+++ js/arrays6.js
@@ -0,0 +1,9 @@
+
+function task6(){
+    
+    let numbers=[100,4,16,5,7,-1,0];
+    console.log(numbers.toString());
+    let elem= numbers.shift();
+    numbers.push(elem);
+    console.log(numbers.toString());
+}  
