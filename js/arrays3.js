5eeb7a5506426c97f735f01939c9e9bf83bf0528
Cr1stal52 <156699952+Cr1stal52@users.noreply.github.com>
2024-03-18 21:14:24 +0300

    task3

---
+++ js/arrays3.js
@@ -0,0 +1,10 @@
+
+function task3(){
+
+    return numbers.every((elem)=>elem%2==0);
+    }
+    //let numbers=[1,4,16,5,7,-1,0];
+    let numbers=[4,16,6,-2,0]; 
+    alert(numbers);
+    alert(task3(numbers)); 
+
