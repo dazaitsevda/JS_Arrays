0314318fa7359d2dcde7f76ad9575b9a5fedee24
Cr1stal52 <156699952+Cr1stal52@users.noreply.github.com>
2024-03-18 21:00:15 +0300

    task1

--- js/arrays.js
+++ js/arrays.js
@@ -1,3 +1,24 @@
 function task1(){
     
+    let numbers=[1,4,16,5,7,-1,0];
+    let res1 =numbers.filter((elem)=>elem%2==0);
+    alert(res1);
+    
+    let res2=res1.reduce(function(sum=0,elem){
+        return sum + Math.sqrt(elem);
+    });
+     alert('Сумма квадратных корней четных чисел равна '+ res2);   
 }
+
+//     let numbers=[1,4,16,5,7,-1,0];
+//     let res = numbers.reduce(function(sum=0,elem){
+//     if (elem%2==0) {
+//         return sum + Math.sqrt(elem)
+//     }
+//     else{
+//         return sum;
+//     }
+// });
+// console.log(res);
+
+// }
